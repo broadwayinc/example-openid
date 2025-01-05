d5df09a811b550f36801674e8477f2a865116489
dev <dev@broadwayinc.com>
2025-01-05 04:20:26 +0000

    update

--- srvc.js
+++ srvc.js
@@ -1,10 +1,6 @@
 // replace service id and owner id
-// const SERVICE_ID = "ap22YyFvVcxK0ShseFEc";
-// const OWNER_ID = "f8e16604-69e4-451c-9d90-4410f801c006";
-const SERVICE_ID = "ap214UHTHgvr8ZLYxckv"
-const OWNER_ID = "4d4a36a5-b318-4093-92ae-7cf11feae989"
-
-const _bleedingEdge = { "hostDomain": "skapi.app", "target_cdn": "d1wrj5ymxrt2ir", "network_logs": true }
+const SERVICE_ID = "ap22YyFvVcxK0ShseFEc";
+const OWNER_ID = "f8e16604-69e4-451c-9d90-4410f801c006";
 
 // replace with your google client id
 const GOOGLE_CLIENT_ID = "838334320600-kf0kriblqvjk350dlvan4t8mt910o40t.apps.googleusercontent.com";
@@ -26,11 +22,10 @@ const skapi = new Skapi(
 
                 document.getElementById('el_bt_logout').hidden = !user;
                 document.getElementById('el_bt_login').hidden = !!user;
-                
+
                 console.log('user', user ? JSON.stringify(user, null, 2) : 'Please Login');
                 document.getElementById('el_pre_userProfile').innerText = user ? JSON.stringify(user, null, 2) : 'Please Login';
             }
         }
-    },
-    _bleedingEdge
+    }
 );
